9de2ed612dd209887a51286551e00a0a1e344adf
Levi Kennedy <kenne09839@students.pvcc.vic.edu.au>
2022-03-04 15:27:16 +1100

    Add project files.

---
+++ SAC 1/Form1.Designer.vb
@@ -0,0 +1,109 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.btnPseudo = New System.Windows.Forms.Button()
+        Me.lblPeople = New System.Windows.Forms.Label()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(91, 156)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(133, 106)
+        Me.btnAdd.TabIndex = 0
+        Me.btnAdd.Text = "Add to CSV"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(91, 99)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(133, 20)
+        Me.txtName.TabIndex = 2
+        '
+        'lblName
+        '
+        Me.lblName.AutoSize = True
+        Me.lblName.Location = New System.Drawing.Point(88, 83)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(136, 13)
+        Me.lblName.TabIndex = 3
+        Me.lblName.Text = "ENTER STUDENT NAME:"
+        '
+        'btnPseudo
+        '
+        Me.btnPseudo.Location = New System.Drawing.Point(159, 278)
+        Me.btnPseudo.Name = "btnPseudo"
+        Me.btnPseudo.Size = New System.Drawing.Size(65, 65)
+        Me.btnPseudo.TabIndex = 6
+        Me.btnPseudo.Text = "Calculate People In Class"
+        Me.btnPseudo.UseVisualStyleBackColor = True
+        '
+        'lblPeople
+        '
+        Me.lblPeople.AutoSize = True
+        Me.lblPeople.Location = New System.Drawing.Point(265, 278)
+        Me.lblPeople.Name = "lblPeople"
+        Me.lblPeople.Size = New System.Drawing.Size(46, 13)
+        Me.lblPeople.TabIndex = 7
+        Me.lblPeople.Text = "People: "
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(91, 278)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(62, 65)
+        Me.btnClear.TabIndex = 8
+        Me.btnClear.Text = "Clear CSV"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.lblPeople)
+        Me.Controls.Add(Me.btnPseudo)
+        Me.Controls.Add(Me.lblName)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents lblName As Label
+    Friend WithEvents btnPseudo As Button
+    Friend WithEvents lblPeople As Label
+    Friend WithEvents btnClear As Button
+End Class
---
+++ SAC 1/Form1.vb
@@ -0,0 +1,89 @@
+﻿Public Class Form1
+    Dim peopleCounter As Integer
+    Dim entry As String = ""
+    Dim currentEntry As String
+
+    Function clearCSV()
+        My.Computer.FileSystem.WriteAllText("E:\repos\marks.csv", "", False)
+    End Function
+
+    Function WriteToCSV(name, mark1, mark2, mark3)
+        If entry = "" Then
+            My.Computer.FileSystem.WriteAllText("E:\repos\marks.csv", ",English,Maths,PE", True)
+        End If
+        entry += vbNewLine + name + "," + mark1 + "," + mark2 + "," + mark3
+        currentEntry = vbNewLine + name + "," + mark1 + "," + mark2 + "," + mark3
+        My.Computer.FileSystem.WriteAllText("E:\repos\marks.csv", currentEntry, True)
+    End Function
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        Dim name = txtName.Text
+        If name = "" Then
+            MsgBox("Please enter a NAME in the text box above.", vbCritical)
+        Else
+            Dim markEnglish = InputBox("Please enter " + name + "'s English mark: ")
+            If IsNumeric(markEnglish) Then
+                Dim markMaths = InputBox("Please enter " + name + "'s Maths mark: ")
+                If IsNumeric(markMaths) Then
+                    Dim markPE = InputBox("Please enter " + name + "'s PE mark: ")
+                    If IsNumeric(markPE) Then
+                        WriteToCSV(name, markEnglish, markMaths, markPE)
+                    Else
+                        MsgBox("Please enter the PE MARK as a NUMBER.", vbCritical)
+                    End If
+                Else
+                    MsgBox("Please enter the MATHS MARK as a NUMBER.", vbCritical)
+                End If
+            Else
+                MsgBox("Please enter the ENGLISH MARK as a NUMBER.", vbCritical)
+            End If
+        End If
+
+    End Sub
+
+    Private Sub btnPseudo_Click(sender As Object, e As EventArgs) Handles btnPseudo.Click
+        Using MyReader As New Microsoft.VisualBasic.FileIO.TextFieldParser("E:\repos\marks.csv")
+            Dim ticker As Integer = 0
+            Dim englishMarks As String = "ENGLISH MARKS: "
+            Dim mathsMarks As String = "MATH MARKS: "
+            Dim peMarks As String = "STUDENTS: "
+            Dim nameList As String = ""
+            peopleCounter = 0
+
+            MyReader.TextFieldType = FileIO.FieldType.Delimited
+            MyReader.SetDelimiters(",")
+            Dim row As String()
+            While Not MyReader.EndOfData
+                row = MyReader.ReadFields()
+                peopleCounter += 1
+                For Each cell In row
+                    ticker += 1
+                    Select Case ticker
+                        Case 1
+                            nameList += cell + ", "
+                        Case 2
+                            englishMarks += cell + ", "
+                        Case 3
+                            mathsMarks += cell + ", "
+                        Case 4
+                            peMarks += cell + ", "
+                    End Select
+                Next
+            End While
+
+            peopleCounter -= 1
+            lblPeople.Text = "The number of people in this class is: " + peopleCounter.ToString()
+        End Using
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        clearCSV()
+        MsgBox("CSV cleared!")
+        entry = ""
+        lblPeople.Text = "The number of people in this class is: 0"
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        lblPeople.Text = "The number of people in this class is: 0"
+    End Sub
+End Class
